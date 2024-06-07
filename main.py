9baa68a7b8975d369e07943c47c85553a9bbaa71
masonmoralez <136012636+masonmoralez@users.noreply.github.com>
2024-06-06 14:50:27 -1000

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ import torchvision.transforms as transforms
 from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader
 
+class NumberRecognizer(nn.Module):
+    def __init__(self):
+        super(NumberRecognizer, self).__init__()
+        self.fc1 = nn.Linear(28*28, 128)
+        # creates a grid that has 28 by 28 inputs (28 is the number of pixels)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 10)
+        # 10 classes (digits 0 through 9)
