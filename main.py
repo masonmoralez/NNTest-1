c183ea89aa755755496b101517488401f0052513
masonmoralez <136012636+masonmoralez@users.noreply.github.com>
2024-06-06 14:47:53 -1000

    Imported the torch files to be used

---
+++ main.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
+
