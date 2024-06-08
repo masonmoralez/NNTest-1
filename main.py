a9c4e14ee85d20ac785b5537f451be851a585155
masonmoralez <136012636+masonmoralez@users.noreply.github.com>
2024-06-07 15:05:09 -1000

    Updated main.py
    Added some more comments to better understand. Will comment more for better understanding.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 # imports the optimization algorithms from pytorch like stochastic gradient descrent
 import torch.optim as optim
 # classes for handling datasets and creating data loaders
-from torch.utils.data import Dataset,DataLoader
+from torch.utils.data import Dataset, DataLoader
 # allows you to normalize your data
 import torchvision.transforms as transforms
 # imports images
@@ -19,17 +19,20 @@ import numpy as np
 
 import csv
 
+
 # Step 2: Define your neural network architecture
-class numberDataset(Dataset):
+class numberDataset (Dataset):
     # initializes the data setting it to the csv_file that is being read
     def __init__(self, csv_file, transform=None):
         # reads in data
         self.data = pd.read_csv(csv_file)
         self.transform = transform
         # returns
+
     def __len__(self):
         return len(self.data)
         # returns the length of the data
+
     def __getitem__(self, idx):
         # takes each row 1 to end and turns it into a 28 x 28 matrix (row 0 is the labels ie pixel 0, pixel 1, etc)
         sample = self.data.iloc[idx].values.astype('uint8').reshape((28, 28))
@@ -38,10 +41,13 @@ class numberDataset(Dataset):
             # transform normalizes the data
         return sample
 
+
 # Load training data
+# this composes several transforms together
+
 transform = transforms.Compose([
-    # Convert to tensor
     transforms.ToTensor(),
+    # Convert to tensor
     # Normalize the pixel values, first number subtracts from original, second number divides
     # makes values between -1 and 1
     transforms.Normalize((.5,), (.5,))
@@ -52,7 +58,7 @@ base_path = os.path.dirname(os.path.abspath(__file__))  # Get the directory of t
 csv_file_path = str(base_path) + r'\data\test.csv'  # Construct the relative path to the CSV file
 
 # load data
-number_dataset = numberDataset(csv_file=csv_file_path,transform=transform)
+number_dataset = numberDataset(csv_file=csv_file_path, transform=transform)
 
 # loads in data 64 at a time and shuffles dataset for each epoch
 dataloader = DataLoader(number_dataset, batch_size=64, shuffle=True)
