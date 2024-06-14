d44b01b25e1e2023f969b0494e9e0a77fd2dd8da
masonmoralez <136012636+masonmoralez@users.noreply.github.com>
2024-06-14 10:17:21 -1000

    changed the code

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import torch
 # imports the neural networks classes and functions from pytorch
 import torch.nn as nn
 
+from train import training
+
 # imports the optimization algorithms from pytorch like stochastic gradient descrent
 # these are interchangeable with each other, just need to refer to each other
 import torch.optim as optim
@@ -29,10 +31,6 @@ import matplotlib.pyplot as plt
 
 # data exploration
 import pandas as pd
-import numpy as np
-
-import csv
-
 
 # Step 2: Define your neural network architecture
 class numberDataset (Dataset):
@@ -63,48 +61,121 @@ class numNN_train(nn.Module):
     # creates and initializes the intial biases
     def __init__(self):
         # call initialization method for the parent class nn.Module
-        super().__init__()
+        super(numNN_train, self).__init__()
         
         ''' tested different initializations with the variables '''
         # # creates weights with value of 0 to be trained
-        # self.w00 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
-        # self.b00 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
-        # self.w01 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
-        # # need to set requires_grad to true in order to train it
+        self.w00 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+        self.b00 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
+        self.w01 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+        # need to set requires_grad to true in order to train it
 
-        # self.w10 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
-        # self.b10 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
-        # self.w11 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
+        self.w10 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+        self.b10 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
+        self.w11 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
 
-        # self.w20 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
-        # self.b20 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
-        # self.w21 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
+        self.w20 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+        self.b20 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
+        self.w21 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
 
-        self.fc1 = nn.Linear(28 * 28, 128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 1)
-        # final bias in this case is right before the output
+        self.w30 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+        self.b30 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
+        self.w31 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+
+        self.w40 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+        self.b40 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
+        self.w41 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+
+        self.w50 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+        self.b50 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
+        self.w51 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+        
+        self.w60 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+        self.b60 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
+        self.w61 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+
+        self.w60 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+        self.b60 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
+        self.w61 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+
+        self.w70 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+        self.b70 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
+        self.w71 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+
+        self.w80 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+        self.b80 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
+        self.w81 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+
+        self.w90 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+        self.b90 = nn.Parameter(torch.tensor(0.0), requires_grad=True)
+        self.w91 = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+
+        self.final_bias = nn.Parameter(torch.tensor(0.0), requires_grad=True)
+
+        # self.fc1 = nn.Linear(28 * 28, 128)
+        # self.fc2 = nn.Linear(128, 64)
+        # self.fc3 = nn.Linear(64, 10)
+        # self.relu = nn.ReLU()
 
     # goes through the neural network by taking an input value and calculating the output value with the weights, biases, and activation functions
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+    def forward(self, input):
+        # x = x.view(-1, 28*28)
+        # x = F.relu(self.fc1(x))
+        # x = F.relu(self.fc2(x))
+        # x = self.fc3(x)
+        # return x
+
+        # x = F.relu(self.fc1(x))
+        # x = F.relu(self.fc2(x))
+        # x = self.fc3(x)
+        # print(x)
+        # return x
         
-        # input_to_top_relu = input * self.w00 + self.b00
-        # top_relu_output = F.relu(input_to_top_relu)
-        # scaled_top_relu_ouput = top_relu_output * self.w01
+        input_to_top_relu = input * self.w00 + self.b00
+        top_relu_output = F.relu(input_to_top_relu)
+        scaled_top_relu_ouput = top_relu_output * self.w01
         
-        # input_to_bottom_relu = input * self.w10 + self.b10
-        # top_bottom_output = F.relu(input_to_bottom_relu)
-        # scaled_bottom_relu_ouput = top_bottom_output * self.w11
+        input_to_bottom_relu = input * self.w10 + self.b10
+        top_bottom_output = F.relu(input_to_bottom_relu)
+        scaled_bottom_relu_ouput = top_bottom_output * self.w11
 
-        # input_to_final_relu = scaled_top_relu_ouput + scaled_bottom_relu_ouput + self.final_bias
+        input_to_bottom_relu = input * self.w20 + self.b20
+        top_bottom_output = F.relu(input_to_bottom_relu)
+        scaled_bottom_relu_ouput = top_bottom_output * self.w21
 
-        # output = F.relu(input_to_final_relu)
+        input_to_bottom_relu = input * self.w30 + self.b30
+        top_bottom_output = F.relu(input_to_bottom_relu)
+        scaled_bottom_relu_ouput = top_bottom_output * self.w31
+        
+        input_to_bottom_relu = input * self.w40 + self.b40
+        top_bottom_output = F.relu(input_to_bottom_relu)
+        scaled_bottom_relu_ouput = top_bottom_output * self.w41
 
-        # return output
+        input_to_bottom_relu = input * self.w50 + self.b50
+        top_bottom_output = F.relu(input_to_bottom_relu)
+        scaled_bottom_relu_ouput = top_bottom_output * self.w51
+
+        input_to_bottom_relu = input * self.w60 + self.b60
+        top_bottom_output = F.relu(input_to_bottom_relu)
+        scaled_bottom_relu_ouput = top_bottom_output * self.w61
+
+        input_to_bottom_relu = input * self.w70 + self.b70
+        top_bottom_output = F.relu(input_to_bottom_relu)
+        scaled_bottom_relu_ouput = top_bottom_output * self.w71
+        
+        input_to_bottom_relu = input * self.w80 + self.b80
+        top_bottom_output = F.relu(input_to_bottom_relu)
+        scaled_bottom_relu_ouput = top_bottom_output * self.w81
+
+        input_to_bottom_relu = input * self.w90 + self.b90
+        top_bottom_output = F.relu(input_to_bottom_relu)
+        scaled_bottom_relu_ouput = top_bottom_output * self.w91
+
+        input_to_final_relu = scaled_top_relu_ouput + scaled_bottom_relu_ouput + self.final_bias
+
+        output = F.relu(input_to_final_relu)
+
+        return output
     
     
 # Load training data
@@ -127,50 +198,9 @@ number_dataset = numberDataset(csv_file=csv_file_path, transform=transform)
 # loads in data 64 at a time and shuffles dataset for each epoch
 dataloader = DataLoader(number_dataset, batch_size=64, shuffle=True)
 
-def train(epochs, input, model, optimizer):
-    for epoch in range(epochs):
-        total_loss = 0
-        # indexing method for going through the code (to update back to original use the indexing method on inputs)
-        # for i in range (len(input)):
-        #     input_i = (input[i][0]).float
-        #     label_i = (input[i][1]).float
-
-        for inputs, labels in dataloader:
-            # The second argument is -1. In PyTorch, using -1 in the view function tells PyTorch 
-            # to infer the size of that dimension based on the total number of elements and the 
-            # other specified dimensions. (why -1 is used)
-
-            inputs = (inputs.view(inputs.size(0), -1)).float()  # Flatten inputs
-            labels = (labels.view(-1,1)).float()
-
-            outputs = model(inputs)
-
-            # Sum of Squared residuals loss function 
-            loss = torch.mean((outputs - labels) ** 2)
-
-            # calculates the gradients for all tensors that require grad
-            loss.backward()
-
-            # adds loss
-            total_loss += loss.item()
-
-            # takes step based on loss
-            optimizer.step()
-
-            # zeros out derivatives
-            optimizer.zero_grad()
-            # breaks function if 
-            if total_loss <= 0.001:
-                print("Number of Steps", str(epoch))
-                break
-
-        print("Step: ", epoch + 1, "Loss: ", total_loss)
-
-
-
 # creates training model
 number_train_model = numNN_train()
 
 optimizer = SGD(number_train_model.parameters(), lr=0.1)
 
-train(50, number_dataset, number_train_model,optimizer)
+training(50, number_train_model,optimizer,dataloader)
---
+++ train.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+def training(epochs,model, optimizer,dataloader):
+    for epoch in range(epochs):
+        total_loss = 0
+        # indexing method for going through the code (to update back to original use the indexing method on inputs)
+        # for i in range (len(input)):
+        #     input_i = (input[i][0]).float
+        #     label_i = (input[i][1]).float
+
+        for inputs, labels in dataloader:
+            # The second argument is -1. In PyTorch, using -1 in the view function tells PyTorch 
+            # to infer the size of that dimension based on the total number of elements and the 
+            # other specified dimensions. (why -1 is used)
+
+            inputs = (inputs.view(inputs.size(0), -1)).float()  # Flatten inputs
+            labels = (labels.view(-1,1)).float()
+
+            outputs = model(inputs)
+
+            # Sum of Squared residuals loss function 
+            loss = torch.mean((outputs - labels) ** 2)
+
+            # calculates the gradients for all tensors that require grad
+            loss.backward()
+
+            # adds loss
+            total_loss += loss.item()
+
+            # takes step based on loss
+            optimizer.step()
+
+            # zeros out derivatives
+            optimizer.zero_grad()
+            # breaks function if 
+            if total_loss <= 0.001:
+                print("Number of Steps", str(epoch))
+                break
+
+        print(model.w00)
+        print("Step: ", epoch + 1, "Loss: ", total_loss)
+
