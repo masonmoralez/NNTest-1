db514c3b3bf5e6f6b50f83d71a7cb1875a797fa6
masonmoralez <moralesmason977@gmail.com>
2024-06-26 12:03:01 -1000

    Optimized_NN.pth Added
    Added code to store the optimized_NN.pth which is the weights and biases values

--- 02test.py
+++ 02test.py
@@ -3,6 +3,7 @@ import os
 import torch
 # imports the neural networks classes and functions from pytorch
 import torch.nn as nn
+import random
 
 from F_train import train_model
 from F_data import CustomDigitDataset
@@ -28,11 +29,13 @@ import matplotlib.pyplot as plt
 # data exploration
 import pandas as pd
 
+batch_size = 200
+
 base_path = os.path.dirname(os.path.abspath(__file__))  # Get the directory of the current script
 train_dataset = CustomDigitDataset(os.path.join(base_path,'train.csv'))
 test_dataset = CustomDigitDataset(os.path.join(base_path,'test.csv'))
-train_loader = DataLoader(train_dataset, batch_size=200, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=200, shuffle=True)
+train_loader = DataLoader(train_dataset, batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size, shuffle=True)
     # - train_dataset (the dataset that we loaded)
     # - batch_size (how many samples will be passed through the data at one time)
     # - shuffle (enabling this with True means that you get random data each time)
@@ -51,5 +54,7 @@ print('Labels:', labels)
 print('Images:', images.numpy())
 
 # Need to pick a manual seed for randomization of the weights and biases that we are using
-torch.manual_seed(11) # 11 is just a random number (in this case I picked it because its Jalen Brunson's number). Can really be any number
-finished_model = train_model(train_loader, test_loader)
+torch.manual_seed(random.randint(1, 100)) # 11 is just a random number (in this case I picked it because its Jalen Brunson's number). Can really be any number
+finished_model = train_model(train_loader, test_loader, batch_size)
+
+torch.save(finished_model.state_dict(), 'optimized_NN.pth')
--- F_train.py
+++ F_train.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 from F_nnModel import numNN_train
 
-def train_model(train_loader, test_loader, learning_rate = 0.001, epochs = 30):
+def train_model(train_loader, test_loader, batch_size, learning_rate = 0.001, epochs = 30):
     num_Model = numNN_train()
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(num_Model.parameters(), lr=learning_rate)
@@ -26,8 +26,8 @@ def train_model(train_loader, test_loader, learning_rate = 0.001, epochs = 30):
             optimizer.step()
             # Accumulate loss
             running_loss += loss.item()
-            if i % 200 == 199:  # Print every 100 mini-batches
-                print(f'Epoch {epoch + 1}, Batch {i + 1}, Loss: {running_loss / 100:.3f}')
+            if i % 200 == 199:  # Print every 200 mini-batches
+                print(f'Epoch {epoch + 1}, Batch Size {batch_size}, Loss: {running_loss / 100:.3f}')
                 running_loss = 0.0
 
     return num_Model 
