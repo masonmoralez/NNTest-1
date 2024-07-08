b9434c7522dafa4c213230ecaa86c05e00e1eb53
yestochickens <109009673+yestochickens@users.noreply.github.com>
2024-07-07 21:12:42 -1000

    clear screen and drawing but its pretty trash

--- F_whiteboard.py
+++ F_whiteboard.py
@@ -1,13 +1,38 @@
-import pygame as py 
+import pygame as p
 
-class WhiteBoard():
-    def main():
-        width = height = 784
-        py.init()
-        screen = py.display.set_mode((width, height))
-        py.display.set_caption("AnimalChess")
-        screen.fill(py.Color(117,117,117))
-        return
-    
-if __name__ == '__main__':
-    WhiteBoard().run()
+p.init()
+
+scale = 20
+screen_width = scale * 28
+screen_height = scale * 28
+
+'''
+Colors
+'''
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+screen = p.display.set_mode((screen_width, screen_height))
+screen.fill(BLACK)
+running = True
+while running:
+    for event in p.event.get():
+        if event.type == p.QUIT:
+            running = False
+        if p.mouse.get_pressed()[0]:
+            # draw
+            mouse_x,mouse_y = p.mouse.get_pos()
+            p.draw.circle(screen, WHITE, (p.mouse.get_pos()), scale // 3)
+            print(p.mouse.get_pos())
+        if event.type == p.KEYDOWN:
+            # clear screen
+            if event.key == p.K_c:
+                screen.fill(BLACK)
+                p.display.flip()
+    p.display.flip()
+
+
+
+
+
+p.quit()
