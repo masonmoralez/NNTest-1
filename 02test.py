49c6eaa9f49c8dba050d6decb0beba130a2f075d
masonmoralez <moralesmason977@gmail.com>
2025-01-07 16:35:59 -1000

    changes

---
+++ 02test.py
@@ -0,0 +1,60 @@
+import os
+# allows us to do tensor math, calculations, and storage of data
+import torch
+# imports the neural networks classes and functions from pytorch
+import torch.nn as nn
+import random
+
+from F_train import train_model
+from F_data import CustomDigitDataset
+
+# imports the optimization algorithms from pytorch like stochastic gradient descrent
+# these are interchangeable with each other, just need to refer to each other
+import torch.nn.functional as F 
+
+# classes for handling datasets and creating data loaders
+from torch.utils.data import DataLoader
+
+# allows you to normalize your data
+import torchvision
+import torchvision.transforms as transforms
+
+# imports images
+from torchvision.datasets import ImageFolder
+
+# pre-trained image models from pytorch
+# data visulization
+#import matplotlib.pyplot as plt
+
+# data exploration
+import pandas as pd
+
+batch_size = 200
+
+base_path = os.path.dirname(os.path.abspath(__file__))  # Get the directory of the current script
+train_dataset = CustomDigitDataset(os.path.join(base_path,'train.csv'))
+test_dataset = CustomDigitDataset(os.path.join(base_path,'test.csv'))
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+    # - train_dataset (the dataset that we loaded)
+    # - batch_size (how many samples will be passed through the data at one time)
+    # - shuffle (enabling this with True means that you get random data each time)
+
+''' Uncomment this code for debugging (purpose is to see the labels and images plus their size)'''
+
+# Print some samples from train_loader
+dataiter = iter(train_loader)
+images, labels = next(dataiter)
+
+print('Images shape:', images.shape)
+print('Labels shape:', labels.shape)
+print('Labels:', labels)
+
+# Optionally, you can print the images in a more readable format (for example, as numpy arrays)
+print('Images:', images.numpy())
+
+# Need to pick a manual seed for randomization of the weights and biases that we are using
+torch.manual_seed(random.randint(1, 100)) # 11 is just a random number (in this case I picked it because its Jalen Brunson's number). Can really be any number
+finished_model = train_model(train_loader, test_loader, batch_size)
+
+torch.save(finished_model.state_dict(), 'optimized_NN.pth') 
